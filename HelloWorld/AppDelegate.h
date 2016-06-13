ec51c8446fedec5f6826aa8666ad45d7684b1bda
Stefan Jauker <s.jauker@mwaysolutions.com>
2016-06-13 11:59:54 +0200

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Stefan Jauker on 13/06/16.
+//  Copyright © 2016 Relution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Stefan Jauker on 13/06/16.
+//  Copyright © 2016 Relution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
